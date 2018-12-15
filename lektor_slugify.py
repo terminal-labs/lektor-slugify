f4cb20d8e9287e8bd25e6ce3d64ddd249a535d45
Joseph Nix <nixjdm@terminallabs.com>
2018-12-15 12:10:50 -0600

    [master] Blackening

--- lektor_slugify.py
+++ lektor_slugify.py
@@ -3,11 +3,13 @@ from lektor.pluginsystem import Plugin
 
 from slugify import slugify
 
+
 class SlugifyPlugin(Plugin):
-    name = 'Slugify'
-    description = u'Lektor plugin that adds a slugify Jinja filter.'
+    name = "Slugify"
+    description = u"Lektor plugin that adds a slugify Jinja filter."
 
     def on_setup_env(self, **extra):
         def slug_filter(txt, **kwargs):
             return slugify(txt, **kwargs)
-        self.env.jinja_env.filters['slug'] = slug_filter
+
+        self.env.jinja_env.filters["slug"] = slug_filter
--- setup.py
+++ setup.py
@@ -4,39 +4,34 @@ import re
 
 from setuptools import setup
 
-with io.open('README.md', 'rt', encoding="utf8") as f:
+with io.open("README.md", "rt", encoding="utf8") as f:
     readme = f.read()
 
-_description_re = re.compile(r'description\s+=\s+(?P<description>.*)')
+_description_re = re.compile(r"description\s+=\s+(?P<description>.*)")
 
-with open('lektor_slugify.py', 'rb') as f:
-    description = str(ast.literal_eval(_description_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open("lektor_slugify.py", "rb") as f:
+    description = str(
+        ast.literal_eval(_description_re.search(f.read().decode("utf-8")).group(1))
+    )
 
 setup(
-    author=u'Terminal Labs, Michael Verhulst, Joseph Nix',
-    author_email='solutions@terminallabs.com',
+    author=u"Terminal Labs, Michael Verhulst, Joseph Nix",
+    author_email="solutions@terminallabs.com",
     description=description,
-    keywords='Lektor plugin static-site slug jinja2 jinja filter',
-    license='BSD-3-Clause',
+    keywords="Lektor plugin static-site slug jinja2 jinja filter",
+    license="BSD-3-Clause",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    name='lektor-slugify',
-    py_modules=['lektor_slugify'],
-    url='https://github.com/terminal-labs/lektor-slugify',
-    version='0.3',
+    long_description_content_type="text/markdown",
+    name="lektor-slugify",
+    py_modules=["lektor_slugify"],
+    url="https://github.com/terminal-labs/lektor-slugify",
+    version="0.3",
     classifiers=[
-        'Environment :: Plugins',
-        'Environment :: Web Environment',
-        'Framework :: Lektor',
-        'License :: OSI Approved :: BSD License',
+        "Environment :: Plugins",
+        "Environment :: Web Environment",
+        "Framework :: Lektor",
+        "License :: OSI Approved :: BSD License",
     ],
-    entry_points={
-        'lektor.plugins': [
-            'slugify = lektor_slugify:SlugifyPlugin',
-        ]
-    },
-    install_requires=[
-        'python-slugify>=1.2.6',
-    ]
+    entry_points={"lektor.plugins": ["slugify = lektor_slugify:SlugifyPlugin"]},
+    install_requires=["python-slugify>=1.2.6"],
 )
